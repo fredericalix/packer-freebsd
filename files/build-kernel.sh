0b0a488f093f137b623fc2f04f38d904469a63b1
fredericalix <frederic.alix@fredalix.com>
2019-08-15 23:02:16 +0200

    add template and scripts files

---
+++ files/build-kernel.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
+export PATH
+
+#ZFS RESIZE
+echo resize
+sudo gpart recover da0
+sudo gpart resize -i 2 /dev/da0
+sudo zpool set autoexpand=on zroot
+sudo zpool online -e zroot da0p2
+echo resize OK
+
+#BUILD KERNEL
+echo build
+cd /usr/src
+sudo git clone https://github.com/freebsd/freebsd.git ./
+cd /usr/src/sys/amd64/conf
+sudo sh -c "echo 'device          pf' >> GENERIC"
+sudo sh -c "echo 'device          pflog' >> GENERIC"
+sudo sh -c "echo 'device          pfsync' >> GENERIC"
+cd /usr/src
+sudo sh -c "echo 'MALLOC_PRODUCTION=YES' > /etc/src.conf"
+sudo sh -c "echo 'WITHOUT_SENDMAIL=YES' >> /etc/src.conf"
+mkdir /var/tmp/log
+sudo make -DMALLOC_PRODUCTION -j2 buildworld 1>/var/tmp/log/buildworld-CURR-$(date +%F).log
+sudo make -DMALLOC_PRODUCTION -j2 buildkernel KERNCONF=GENERIC-NODEBUG 1>/var/tmp/log/buildkernel-CURR-$(date +%F).log
+sudo make -j2 installkernel KERNCONF=GENERIC-NODEBUG 1>/var/tmp/log/install-kernel-CURR-$(date +%F).log
+sudo make -j2 installworld KERNCONF=GENERIC-NODEBUG 1>/var/tmp/log/install-world-CURR-$(date +%F).log
+
+#PURGE
+cd /
+sudo zfs destroy -f zroot/usr/src
+
+#UPDATE PKG
+sudo pkg update
+sudo pkg upgrade -y
